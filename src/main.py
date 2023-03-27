f1f7850190394e156501d267631686b20bbf437b
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-03-27 03:10:08 +0300

    Added Russian translation

--- src/main.py
+++ src/main.py
@@ -104,7 +104,7 @@ class AsciiimagesApplication(Adw.Application):
         # This is a Python list: Add your string to the list (separated by a comma)
         devs_list = ['gregorni https://gitlab.com/gregorni']
         # This is a string: Add your name to the string (separated by a newline '\n')
-        translators_list = 'gregorni https://gitlab.com/gregorni\nIrénée Thirion'
+        translators_list = 'gregorni https://gitlab.com/gregorni\nIrénée Thirion\nFyodor Sobolev https://github.com/fsobolev'
 
         
         """Callback for the app.about action."""
